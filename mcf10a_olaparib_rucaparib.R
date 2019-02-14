8742eaaa0910954d65504e73fc790b2eeaa28698
David J. H. Shih <djh.shih@gmail.com>
2019-02-14 17:36:55 -0600

    Added example files.

---
+++ mcf10a_olaparib_rucaparib.R
@@ -0,0 +1,46 @@
+source("R/common.R");
+
+cell_line <- "MCF10A";
+compounds <- c("Olaparib", "Rucaparib");
+
+data_t0_fname <- "tsv/mcf10a_d0_i1_2019-02-08.tsv";
+data_t_fname <- "tsv/mcf10a_d5_i1_olaparib_rucaparib_2019-02-13.tsv";
+
+
+date.format <- "%Y-%m-%d %H:%M";
+time_t0 <- strptime("2019-02-08 16:00", format=date.format);
+time_t <- strptime("2019-02-13 15:00", format=date.format);
+
+assay <- list(
+	method = "Presto Blue, fluorescence",
+	incubation_hours = 1,
+	duration_hours = as.numeric(difftime(time_t, time_t0, units="hours"))
+);
+
+annot_fname <- "annot/cell-viability-drug_2019-02-08b.csv";
+
+design_t0_fname <- "annot/plate-design_d0.tsv";
+design_t_fname <- "annot/plate-design_d5_2drugs.tsv";
+
+out_dir <- "norm";
+
+ys <- normalize_viability(
+	data_t0_fname, data_t_fname,
+	design_t0_fname, design_t_fname,
+	annot_fname,
+	cell_line,
+	compounds,
+	assay
+);
+
+lapply(compounds, function(cp) {
+		qwrite(ys[[cp]],
+			filename("viability",
+				path=out_dir,
+				tag=c(tolower(cell_line), tolower(cp)),
+				ext="rds"
+			)
+		);
+	}
+);
+
